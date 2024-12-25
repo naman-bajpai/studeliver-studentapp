a71cb7521316aa40c9aee44ff5204867787909af
potato-lulw <om.patil17072@gmail.com>
2024-12-25 20:33:42 +0530

    Addes assets

---
+++ constants/exportsImages.ts
@@ -0,0 +1,6 @@
+import logo_transparent from '@/assets/images/studeliverlogo.png'
+import logo_whitebg from '@/assets/images/studeliver_whitebg.png'
+
+export default {
+    logo_transparent, logo_whitebg
+}
