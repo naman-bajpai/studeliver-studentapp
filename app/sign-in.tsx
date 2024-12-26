e6b999aa5fe9c95701772abd84a1e5bbc3291255
potato-lulw <om.patil17072@gmail.com>
2024-12-27 03:47:25 +0530

    Added a welcome greeting and some style changes to signup page

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -4,8 +4,9 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { ScrollView } from 'react-native'
 import images from '@/constants/exportsImages.ts'
 import { StatusBar } from 'expo-status-bar'
+import { router } from 'expo-router'
 const SignIn = () => {
-  const { width } = Dimensions.get('window');
+  
   return (
 
     <SafeAreaView className='bg-white h-full font-okra' >
@@ -21,20 +22,20 @@ const SignIn = () => {
 
         
 
-        <View className='z-10 bg-accent mx-5 w-[90%]  mt-28 rounded-3xl p-10 shadow shadow-black elevation-md'>
+        <View className='z-10 bg-accent mx-5 w-[90%]  mt-28 rounded-3xl p-10 pt-5 shadow shadow-black elevation-md'>
           <Image source={images.logo_transparent_cropped} alt='studeliver-logo'  className='' style={{ height: 100, width: "100%", resizeMode: "contain" }} />
-          
+          <Text className='font-okra-medium text-center text-2xl '>Serving up delicious moments{"\n"}<Text className='font-okra-bold text-primary'>Join us!</Text></Text>
           <View className="mt-6">
             {/* <Text className="text-lg font-semibold mb-2">Username</Text> */}
             <TextInput
               placeholder="Enter your username"
-              className="font-okra border border-gray-300 p-4 rounded-3xl mb-4 focus:border-primary focus:outline-none "
+              className="font-okra border border-gray-500 p-4 rounded-3xl mb-4 focus:border-primary focus:outline-none "
             />
             {/* <Text className="text-lg font-semibold mb-2">University Email</Text> */}
             <TextInput
               placeholder="Enter your university email"
               keyboardType="email-address"
-              className="font-okra border border-gray-300 p-4 rounded-3xl mb-6 focus:border-primary focus:outline-none"
+              className="font-okra border border-gray-500 p-4 rounded-3xl mb-6 focus:border-primary focus:outline-none"
             />
           </View>
 
@@ -68,11 +69,18 @@ const SignIn = () => {
             <Text className='font-okra bg-accent px-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Or</Text>
           </View>
           <View className="mt-8">
-            <Text
+            <Pressable
+              onPress={() => router.push("/")}
+              className=""
+            >
+
+            <Text 
               className="bg-accent text-center text-black-300 border border-primary py-3 rounded-3xl font-okra-bold"
             >
                Enter as a Guest! 
+               {/* Add on click listeneer to navigate to / */}
             </Text>
+            </Pressable>
           </View>
         </View>
 
